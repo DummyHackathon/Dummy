ad744b68134c35fefd59c3cb598854ade14971b4
ltvkhanh1994@gmail.com <ltvkhanh1994@gmail.com>
2016-05-28 13:04:25 +0700

    Add code for GameController

---
+++ Dummy_Hackathon2016/Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public static GameController Instance { get; set; }
+
+    public int numberOfTouch = 0; //Số lượng touch yêu cầu.
+    public float timeToTouch = 0.0f; //Thời gian tối đa để touch
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public bool CheckTouch(int touch)
+    {
+        if (touch == numberOfTouch)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool CheckTime()
+    {
+        if (timeToTouch == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+}
